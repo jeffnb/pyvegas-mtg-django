7b19a8d7e2676003eac961a96b2e6327ba29eadf
jeffnb <jeffnb@pm.me>
2022-07-12 15:07:32 -0700

    Initial commit

--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from cards.models import Card, Color, Edition
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code')
+
+@admin.register(Edition)
+class EditionsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code')
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'mana_cost', 'type', 'rarity', 'set_name')
+    list_filter = ('type', 'rarity', 'set_name')
+
+
+
--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,51 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class Color(models.Model):
+    RED = 'R'
+    GREEN = 'G'
+    BLUE = 'U'
+    WHITE = 'W'
+    BLACK = 'B'
+    COLOR_CHOICES = [(RED, 'Red'), (GREEN, 'Green'), (BLUE, 'Blue'), (WHITE, 'White'), (BLACK, 'Black')]
+
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=3, choices=COLOR_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+class Edition(models.Model):
+    """
+    These are constantly added to so we won't lock them into choice fields
+    """
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Card(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    mana_cost = models.CharField(max_length=100, null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+    flavor = models.TextField(null=True, blank=True)
+    type = models.CharField(max_length=100)
+    power = models.CharField(max_length=5)
+    toughness = models.CharField(max_length=5)
+    rarity = models.CharField(max_length=20)
+    set_name = models.CharField(max_length=50)
+    image_url = models.URLField(max_length=300)
+    edition = models.ForeignKey(Edition, on_delete=models.CASCADE)
+    colors = models.ManyToManyField(Color)
+
+
+    def __str__(self):
+        return self.name
+
