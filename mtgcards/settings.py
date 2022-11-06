fdfa568c40285b9f15e3622f08a09809e5b0669c
jeffnb <jeffnb@pm.me>
2022-11-05 17:05:33 -0700

    using pathlib

--- mtgcards/settings.py
+++ mtgcards/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_hosting')
+STATIC_ROOT = BASE_DIR / 'static_hosting'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
